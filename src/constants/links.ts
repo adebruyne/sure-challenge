81be022ffee8119d005759ec6995b27761cb948d
Aylin DeBruyne <adebruyne88@gmail.com>
2022-02-20 18:35:05 -0500

    adds a home route and link just for the doggo in the wig - too good to be hidden

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,10 @@
 const links = [
   {
+    text: 'Home',
+    href: '/',
+    'data-testid': 'home_link',
+  },
+  {
     text: 'Policyholders',
     href: '/policyholders',
     'data-testid': 'policyholders_link',
