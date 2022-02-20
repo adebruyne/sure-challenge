d3105f6aa75aee5d1c922a27226e3b0746e54d51
Aylin DeBruyne <adebruyne88@gmail.com>
2022-02-20 10:00:13 -0500

    adds active class and replaces NavLink with Link and adds styles to display active - still needs aria-current added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,45 @@
-import { Link, Box } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components/macro'
+
+const NavBarContainer = styled(Box)`
+ background-color: #0c2975;
+ padding: 16px;
+ width: 200px;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+
+ & .navbar_img_link {
+  margin-bottom: 80px;
+   margin-top: 40px;
+    color: '';
+ }
+
+ & .navbar__link{
+   color: #fff;
+   cursor: pointer;
+   text-decoration: none;
+   margin-bottom: 16px; 
+ }
+
+ & .navbar__link:hover{
+      text-decoration: underline
+ }
+
+ & .navbar__link--active{
+   color: #0c2975;
+   background: #fff;
+   text-decoration: underline;
+   margin-bottom: 16px; 
+   border-radius: 40px;
+   border: 1px dotted black;
+    padding-left: 10px;
+    padding-right: 10px;
+ 
+ }
+`
+
 type TNavBar = {
   links: {
     text: string;
@@ -10,44 +50,29 @@ type TNavBar = {
 
 function NavBar({ links }: TNavBar) {
   return (
-    <Box
+    <NavBarContainer
       component="aside"
-      sx={{
-        background: '#0c2975',
-        padding: '16px',
-        width: '200px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
     >
-      <Link
-        component={RouterLink}
+      <NavLink
         to="/"
-        sx={{ cursor: 'pointer', marginBottom: '80px', marginTop: '40px' }}
+        className={'navbar_img_link'}
       >
         <img src="/surelogo.svg" alt="logo"></img>
-      </Link>
-
+      </NavLink>
       {links.map(({ text, href, 'data-testid': dataTestId }) => (
-        <Link
-          component={RouterLink}
-          key={href}
-          to={href}
-          color="#fff"
-          underline="hover"
-          sx={{
-            cursor: 'pointer',
-            '&:not(:last-of-type)': {
-              marginBottom: '16px',
-            },
-          }}
-          data-testid={dataTestId}
-        >
-          {text}
-        </Link>
-      ))}
-    </Box>
+        <>
+          <NavLink
+            key={href}
+            to={href}
+            className={({ isActive }) => isActive ? "navbar__link--active" : "navbar__link"}
+            data-testid={dataTestId}
+          >
+            {text}
+          </NavLink>
+        </>
+      ))
+      }
+    </NavBarContainer >
   );
 }
 
